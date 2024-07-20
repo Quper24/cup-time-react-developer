5c19e4d4b9db638513a4f26b598c9fd99aee8531
Maksim Leskin <maksim.leskin@methed.ru>
2024-07-20 23:33:01 +0300

    add api

--- src/modules/Products.jsx
+++ src/modules/Products.jsx
@@ -1,16 +1,29 @@
-import { products } from "../products";
+import { useEffect } from "react";
+
 import { Product } from "./Product";
+import { useProducts } from "../context/ProductContext";
+import { useSearchParams } from "react-router-dom";
 
-export const Products = () => (
-  <section className="products">
-    <div className="container">
-      <h2 className="products__title">Чай</h2>
+export const Products = () => {
+  const [seachParams] = useSearchParams();
+  const { products, setCategory } = useProducts();
+  const category = seachParams.get("category");
 
-      <ul className="products__list">
-        {products.map((item) => (
-          <Product key={item.id} data={item} />
-        ))}
-      </ul>
-    </div>
-  </section>
-);
+  useEffect(() => {
+    setCategory(category);
+  }, [category, setCategory]);
+
+  return (
+    <section className="products">
+      <div className="container">
+        <h2 className="products__title">Чай</h2>
+
+        <ul className="products__list">
+          {products.map((item) => (
+            <Product key={item.id} data={item} />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
